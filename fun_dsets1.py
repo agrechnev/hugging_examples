f580d9a2d0b90107b696759746fd0403c62bec41
Oleksiy Grechnyev <shrike4625@gmail.com>
2023-03-22 11:03:48 +0200

    fun_dsets1

--- fun_dsets1.py
+++ fun_dsets1.py
@@ -33,17 +33,46 @@ def main():
 
     # Indexing the dataset
     print('\nDataset Indexing')
-    print('dset[0]=', dset[0])     # dict
-    print("dset['label'][:10]=", dset['label'][:10])   # list
+    print('dset[0]=', dset[0])  # dict
+    print("dset['label'][:10]=", dset['label'][:10])  # list
     print('dset[:2]=', dset[:2])  # dict of lists, not Dataset object !!!!
     # Selection: create a Dataset object as a slice
     dset_sel = dset.select(range(10))
     print('dset_sel=', dset_sel)
 
-    # Dataset tokenizing
-    print('\nDataset Tokenizing')
-    tokenizer = transformers.AutoTokenizer.from_pretrained('bert-base-uncased')
-    
+    # Dataset transformatons
+    print('\nDataset Transformation 1')
+    # Tokenize the entire dataset
+    tokenizer = transformers.AutoTokenizer.from_pretrained('distilbert-base-uncased-finetuned-sst-2-english')
+    dset_mapped1 = dset.map(lambda x: tokenizer(x['text']), batched=True)
+    # Calculate the length of each text
+    dset_mapped2 = dset.map(lambda x: {'text_len': len(x['text'])}, batched=False)
+    # Note: the original object dset is not modified !
+    print('dset=', dset)
+    print('dset_mapped1=', dset_mapped1)
+    print('dset_mapped2=', dset_mapped2)
+
+    print('\nDataset Transformation 2')
+    # set_transform() adds a transform on the fly
+    dset.set_transform(lambda x: tokenizer(x['text']))
+    print(dset)  # Still ['text', 'label']
+    # Replaces the columns with ['input_ids', 'attention_mask'], ['text', 'label'] are rrmeoved !
+    print(dset[0:2].keys())
+    dset.set_transform(lambda x: x)  # Clear the transform (don't know if there are better ways)
+    print(dset[0:2].keys())
+
+    print('\nDataset Transformation 3')
+    print(dset_mapped1[0].keys())  # ['text', 'label', 'input_ids', 'attention_mask']
+    # Now, let's set Pytorch output format
+    dset_mapped1.set_format(type='torch', columns=['label', 'input_ids', 'attention_mask'])
+    print(dset_mapped1[0])
+    # However, for slices we still get lists of tensors
+    # print(dset_mapped1[0:2])
+    # Delete a column
+    dset_mapped1.remove_columns('text')
+    print(dset_mapped1[0].keys())  #   ['label', 'input_ids', 'attention_mask']
+    print(dset[0].keys())   # It is still in dset
+
 
 
 ########################################################################################################################
